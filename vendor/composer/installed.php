71a6e81bed741f70ae0df09b96be51d954b73a9c
Adolfo Oropeza <heimdalsoftware2021@gmail.com>
2022-10-02 13:21:17 -0400

    update
    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'heimdalsoftware/router',
+        'pretty_version' => '1.0.0-alpha3',
+        'version' => '1.0.0.0-alpha3',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'heimdalsoftware/router' => array(
+            'pretty_version' => '1.0.0-alpha3',
+            'version' => '1.0.0.0-alpha3',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
